435a474f413c182db05ae8995358bf74b1aa1550
Delasi Mensah <mensadelasi@gmail.com>
2023-09-05 21:12:10 +0000

    added clerk authentication

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,13 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import { ReactNode, FC } from "react";
+
+type AuthLayoutProps = {
+  children: ReactNode;
 };
 
-export default Layout;
+const AuthLayout: FC<AuthLayoutProps> = ({ children }) => {
+  return (
+    <div className="flex items-center justify-center h-full">{children}</div>
+  );
+};
+
+export default AuthLayout;
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 const RootPage = () => {
-  return <div>RootPage</div>;
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default RootPage;
